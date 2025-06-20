ce6efb9eb7977e74748f1ff55ba99c64d50c2a07
SantiCortes2250 <santicortesrincon15@gmail.com>
2025-06-20 21:48:10 +0200

    Fix CORS for Netlify frontend

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+    'http://localhost:5173',
+    'https://tasks3-app.netlify.app',
+    ],
     'supports_credentials' => true,
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
